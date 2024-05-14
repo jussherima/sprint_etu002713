8e5aa9d429a0674ba7484821ffaaf058c2a22193
jussherima <jussherima@gmail.com>
2024-05-14 11:58:55 +0300

    ajout du sprint 1

--- src/servlet/MainServlet.java
+++ src/servlet/MainServlet.java
@@ -1,7 +1,12 @@
 package servlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
+import annotation.Controller;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,9 +23,39 @@ public class MainServlet extends HttpServlet {
         processRequest(request, response);
     }
 
+    @SuppressWarnings("unchecked")
     public void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().println("bienvenu dans le controller");
+        PrintWriter out = response.getWriter();
+                response.getWriter().println("bienvenu dans le controller");
         response.getWriter().println("voici votre url " + request.getRequestURI());
+        String url = request.getRequestURI();
+        System.out.println("url = /framework_test/ ="+url);
+        if (url.equals("/framework_test/")){
+            List<Class<?>> liste_class = getClasses("controller");
+            for(Class clazz : liste_class ){
+                clazz.isAnnotationPresent(Controller.class);
+                out.println("voici un controller"+clazz.getName());
+            }
+        }
+    }
+
+    public List<Class<?>> getClasses(String packageName) {
+        List<Class<?>> classes = new ArrayList<>();
+        String packagePath = packageName.replace('.', '/');
+        File directory = new File(Thread.currentThread().getContextClassLoader().getResource(packagePath).getFile());
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isFile() && file.getName().endsWith(".class")) {
+                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                try {
+                    Class<?> clazz = Class.forName(className);
+                    classes.add(clazz);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return classes;
     }
 }
