80ceb7f442d26c17b480315e7f564dccde26e015
jussherima <jussherima@gmail.com>
2024-06-07 11:23:23 +0300

    ajout sprint 5

--- src/servlet/FrontServlet.java
+++ src/servlet/FrontServlet.java
@@ -8,9 +8,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import annotation.GET;
+import exception.ControllerFolderNotFoundException;
+import exception.DuplicateUrlException;
+import exception.InvalideFunctionRetourException;
+import exception.NoSuchUrlExcpetion;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
@@ -23,27 +26,39 @@ import util.ModelView;
 // @WebServlet(urlPatterns = "/*", name = "monservlet")
 public class FrontServlet extends HttpServlet {
     List<String> liste_controller;
-    HashMap<String,Mapping> mon_map;
+    HashMap<String, Mapping> mon_map;
+    Exception error = null;
 
     // intialization du servlet
     @SuppressWarnings("unchecked")
-    public void init(ServletConfig config) throws ServletException{
+    public void init(ServletConfig config) throws ServletException {
         super.init(config);
         liste_controller = new ArrayList<>();
-        mon_map =new HashMap<>();
+        mon_map = new HashMap<>();
 
         // maka ny fonction si class annoter rehetra
-        String context_Valeur  = getServletConfig().getInitParameter("controller");
-        List<Class<?>> liste_class = getClasses(context_Valeur);
-        for(@SuppressWarnings("rawtypes") Class clazz : liste_class ){
-            if(clazz.isAnnotationPresent(annotation.Controller.class)){
+        String context_Valeur = getServletConfig().getInitParameter("controller");
+        List<Class<?>> liste_class = new ArrayList<>();
+        try {
+            liste_class = getClasses(context_Valeur);
+        } catch (ControllerFolderNotFoundException ex) {
+            error = ex;
+        }
+
+        for (@SuppressWarnings("rawtypes")
+        Class clazz : liste_class) {
+            if (clazz.isAnnotationPresent(annotation.Controller.class)) {
                 liste_controller.add(clazz.getName());
-                mon_map.putAll(getAnnoteMethods(clazz));
-            };
+                try {
+                    mon_map.putAll(getAnnoteMethods(clazz));
+                } catch (DuplicateUrlException exc) {
+                    error = exc;
+                }
+            }
         }
 
     }
-    
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -52,91 +67,96 @@ public class FrontServlet extends HttpServlet {
         processRequest(request, response);
     }
 
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // fonction qui traite toutes les requettes du client
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         // flux de sortie vers le navigateur
         PrintWriter out = response.getWriter();
 
         // prendre l'url clicker
-        
-        String path = request.getRequestURI().split("/")[2];
-        try{
-            if(mon_map.containsKey(path)){
-                invoke_method(path,request,response);
-            }else{
-                out.println("voici le path "+path);
-                // RequestDispatcher req = request.getRequestDispatcher("/view");
-                for(Map.Entry<String, Mapping> entry  : mon_map.entrySet()){
-                    out.println(path+"ve == amin'ny "+entry.getKey());
-                }
+        if (error == null) {
+            String path = request.getRequestURI().split("/")[2];
+            try {
+                invoke_method(path, request, response);
+            } catch (NoSuchUrlExcpetion e) {
+                erreur(out, 2, e);
+            } catch (InvalideFunctionRetourException e) {
+                erreur(out, 3, e);
+            } catch (Exception e) {
+                out.println("misy erreur le izy:" + e.getMessage());
             }
-        }catch(Exception e){
-            out.println("misy erreur le izy:"+e.getMessage());
+        } else {
+            if (error instanceof ControllerFolderNotFoundException) {
+                erreur(out, 2);
+            } else if (error instanceof DuplicateUrlException) {
+                erreur(out, 1);
+            }
         }
-    }   
-
-
-
+    }
 
     // afficher l'url
-    private void show_url_map(String url,HttpServletRequest request,HttpServletResponse response) throws IOException{
+    private void show_url_map(String url, HttpServletRequest request, HttpServletResponse response) throws IOException {
         boolean url_existe = false;
         PrintWriter out = response.getWriter();
-        for(Map.Entry<String,Mapping> entry : this.mon_map.entrySet()){
-            String valeur_url = "/framework_test/"+entry.getKey();
-            if(valeur_url.equals(url)){
+        for (Map.Entry<String, Mapping> entry : this.mon_map.entrySet()) {
+            String valeur_url = "/framework_test/" + entry.getKey();
+            if (valeur_url.equals(url)) {
                 url_existe = true;
                 out.println("====================================================");
-                out.println("nom du class est ="+entry.getValue().getClassName());
-                out.println("nom du method ="+entry.getValue().getMethodName());
+                out.println("nom du class est =" + entry.getValue().getClassName());
+                out.println("nom du method =" + entry.getValue().getMethodName());
                 out.println("====================================================");
             }
         }
-        if(!url_existe){
+        if (!url_existe) {
             out.println("aucun method associer a cette url");
         }
     }
-    
+
     // proceder l'url lannotation
-    @SuppressWarnings
-    private void invoke_method(String url,HttpServletRequest req,HttpServletResponse res) throws Exception{
+    private void invoke_method(String url, HttpServletRequest req, HttpServletResponse res)
+            throws NoSuchUrlExcpetion, InvalideFunctionRetourException, Exception {
         boolean url_existe = false;
 
-        for(Map.Entry<String,Mapping> entry : this.mon_map.entrySet()){
+        for (Map.Entry<String, Mapping> entry : this.mon_map.entrySet()) {
             String valeur_url = entry.getKey();
-            res.getWriter().println(valeur_url+" ve egale a "+url);
-            if(valeur_url.equals(url)){
+            if (valeur_url.equals(url)) {
                 url_existe = true;
                 // prendre la class avec son nom
                 Class<?> clazz = Class.forName(entry.getValue().getClassName());
 
-                Method m = clazz.getDeclaredMethod(entry.getValue().getMethodName(),null);
-                
+                Method m = clazz.getDeclaredMethod(entry.getValue().getMethodName(), null);
+
                 @SuppressWarnings("deprecation")
-                Object retour = m.invoke(clazz.newInstance(),null);
-                
+                Object retour = m.invoke(clazz.newInstance(), null);
+
                 // dans le cas ou le retour de la method est une string
-                res.getWriter().println("mon type de donner est"+retour.getClass());
-                if(retour.getClass() == String.class){
-                    res.getWriter().println((String)retour);
-                }else if(retour.getClass() == ModelView.class){
+                if (retour.getClass() == String.class) {
+                    res.getWriter().println((String) retour);
+                } else if (retour.getClass() == ModelView.class) {
                     res.getWriter().println("l'instance de la class est une view");
-                    trait_view(req, res, (ModelView)retour);
+                    trait_view(req, res, (ModelView) retour);
+                } else {
+                    throw new InvalideFunctionRetourException("retour du fonction invalide");
                 }
                 break;
             }
         }
-        if(!url_existe){
-            res.getWriter().println("aucun method associer a cette url");
+        if (!url_existe) {
+            // res.getWriter().println("aucun method associer a cette url");
+            throw new NoSuchUrlExcpetion("l'url que vous avez saisie n'existe pas");
+
         }
     }
 
     // traitement du modelAndView
-    private void trait_view(HttpServletRequest request,HttpServletResponse response,ModelView view) throws ServletException, IOException{
+    private void trait_view(HttpServletRequest request, HttpServletResponse response, ModelView view)
+            throws ServletException, IOException {
         // creer une dispatcher de servelt
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/"+view.getName());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/" + view.getName());
 
         // affecter les data dans le ModelView vers le dispatcher
-        for(Map.Entry<String,Object> entry : view.getData().entrySet()){
+        for (Map.Entry<String, Object> entry : view.getData().entrySet()) {
             request.setAttribute(entry.getKey(), entry.getValue());
         }
 
@@ -145,10 +165,18 @@ public class FrontServlet extends HttpServlet {
     }
 
     // prendere toute les class dans le package specifier
-    private List<Class<?>> getClasses(String packageName) {
+    private List<Class<?>> getClasses(String packageName) throws ControllerFolderNotFoundException {
         List<Class<?>> classes = new ArrayList<>();
         String packagePath = packageName.replace('.', '/');
-        File directory = new File(Thread.currentThread().getContextClassLoader().getResource(packagePath).getFile());
+        File directory = null;
+        try {
+            directory = new File(Thread.currentThread().getContextClassLoader().getResource(packagePath).getFile());
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new ControllerFolderNotFoundException("le controlleur que vous avez specifier n'existe pas");
+        }
+
         File[] files = directory.listFiles();
         for (File file : files) {
             if (file.isFile() && file.getName().endsWith(".class")) {
@@ -165,16 +193,38 @@ public class FrontServlet extends HttpServlet {
     }
 
     // prendre tou les mthod annoter dans cette class
-    private HashMap<String,Mapping> getAnnoteMethods(Class<?> my_class){
+    private HashMap<String, Mapping> getAnnoteMethods(Class<?> my_class) throws DuplicateUrlException {
         Method[] liste_method = my_class.getDeclaredMethods();
-        HashMap<String,Mapping> liste_annoted_method = new HashMap<>();
-        for(Method m : liste_method){
-            if(m.isAnnotationPresent(GET.class)){
+        HashMap<String, Mapping> liste_annoted_method = new HashMap<>();
+        for (Method m : liste_method) {
+            if (m.isAnnotationPresent(GET.class)) {
                 String my_url = m.getAnnotation(GET.class).url();
-                liste_annoted_method.put(my_url,new Mapping(my_class.getName(), m.getName()));
+                if (liste_annoted_method.containsKey(my_url)) {
+                    throw new DuplicateUrlException("l'url " + my_url + " contient 2 fonction en meme temps ");
+                }
+                liste_annoted_method.put(my_url, new Mapping(my_class.getName(), m.getName()));
             }
         }
         return liste_annoted_method;
     }
-    
+
+    /*
+     * 
+     * fonction qui affiche les erreurs
+     * 
+     */
+    private void erreur(PrintWriter out, int numero) {
+        out.println("<h2>erreur " + numero + "</h1>");
+        out.println("<a style=\"color:red\"  >" + error.getMessage() + "</a>");
+        out.println("<a style=\"color:red\"  >");
+        error.printStackTrace(out);
+    }
+
+    private void erreur(PrintWriter out, int numero, Exception error) {
+        out.println("<h2>erreur " + numero + "</h1>");
+        out.println("<a style=\"color:red\"  >" + error.getMessage() + "</a>");
+        out.println("<a style=\"color:red\"  >");
+        error.printStackTrace(out);
+    }
+
 }
